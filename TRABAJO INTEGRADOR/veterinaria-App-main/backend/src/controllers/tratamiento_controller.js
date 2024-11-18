de719381cb8c53d281a04c99a44ceb2e84f5bf04
Márquez Rivero <yaninamarquez090@gmail.com>
2024-11-18 17:40:23 -0300

    Update tratamiento_controller.js

--- TRABAJO INTEGRADOR/veterinaria-App-main/backend/src/controllers/tratamiento_controller.js
+++ TRABAJO INTEGRADOR/veterinaria-App-main/backend/src/controllers/tratamiento_controller.js
@@ -1,5 +1,5 @@
 
-import mongoose from "Mysql";
+import Mysql from "Mysql";
 import Tratamiento from "../models/Tratamiento.js"
 
 
@@ -9,7 +9,7 @@ import Tratamiento from "../models/Tratamiento.js"
 // Método para ver el detalle del tratamiento
 const detalleTratamiento = async(req,res)=>{
     const {id} = req.params
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese tratamiento`});
+    if( !Mysql.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese tratamiento`});
     const tratamiento = await Tratamiento.findById(id).populate('paciente','_id nombre')
     res.status(200).json(tratamiento)
 }
@@ -19,7 +19,7 @@ const detalleTratamiento = async(req,res)=>{
 // Método para crear el tratamiento
 const registrarTratamiento = async (req,res)=>{
     const {paciente} = req.body
-    if( !mongoose.Types.ObjectId.isValid(paciente) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
+    if( !Mysql.Types.ObjectId.isValid(paciente) ) return res.status(404).json({msg:`Lo sentimos, debe ser un id válido`});
     const tratamiento = await Tratamiento.create(req.body)
     res.status(200).json({msg:`Registro exitoso del tratamiento ${tratamiento._id}`,tratamiento})
 }
@@ -31,7 +31,7 @@ const registrarTratamiento = async (req,res)=>{
 const actualizarTratamiento = async(req,res)=>{
     const {id} = req.params
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tratamiento ${id}`})
+    if( !Mysql.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tratamiento ${id}`})
     await Tratamiento.findByIdAndUpdate(req.params.id,req.body)
     res.status(200).json({msg:"Actualización exitosa del tratamiento"})
 }
@@ -44,7 +44,7 @@ const actualizarTratamiento = async(req,res)=>{
 const eliminarTratamiento = async(req,res)=>{
     const {id} = req.params
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese tratamiento`})
+    if( !Mysql.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe ese tratamiento`})
     await Tratamiento.findByIdAndDelete(req.params.id)
     res.status(200).json({msg:"Tratamiento eliminado exitosamente"})
 }
